a9f3afed78bbaf770300543f8555cc321f16c8b0
Aswin P Ajayan <aswinp2008@gmail.com>
2020-06-16 13:05:41 +0530

    conforming to PEP8

--- scripts/read_laser_data.py
+++ scripts/read_laser_data.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 import rospy
-import roslib
 from rospy_tutorials.msg import Floats
 from rospy.numpy_msg import numpy_msg
 from sensor_msgs.msg import LaserScan
@@ -11,30 +10,23 @@ import numpy as np
 bearings = []
 
 def clbk_laser(msg):
+    """ call back to process laser data"""
     global bearings
-    # 720/5 = 144
-   # regions = [ 
-   #   min(msg.ranges[0:143]),
-   #   min(msg.ranges[144:287]),
-   #   min(msg.ranges[288:431]),
-   #   min(msg.ranges[432:575]),
-   #   min(msg.ranges[576:719]),
-   #  ]
-   # rospy.loginfo(regions)
     reading_indeces = argrelextrema(np.array(msg.ranges), np.less)
     bearings = (np.asarray(reading_indeces, dtype=np.float) * np.pi / 720) - np.pi/2
     rospy.loginfo(bearings)
     rospy.loginfo(reading_indeces)
 
 def main():
+    """ main method declares publishers and subscribers """
     global bearings
     rospy.init_node('reading_laser')
-    sub= rospy.Subscriber("/m2wr/laser/scan", LaserScan, clbk_laser)
+    sub = rospy.Subscriber("/m2wr/laser/scan", LaserScan, clbk_laser)
     pub = rospy.Publisher('/range_readings', numpy_msg(Floats), queue_size=10)
-    rate = rospy.Rate(5) 
+    rate = rospy.Rate(5)
 
     while not rospy.is_shutdown():
-        pub.publish(np.array(bearings,dtype=np.float)) 
+        pub.publish(np.array(bearings, dtype=np.float))
         rate.sleep()
 
 if __name__ == '__main__':
