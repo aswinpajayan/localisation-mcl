f4b492313b932260af433cdf5b593185cbd88611
Aswin P Ajayan <aswinp2008@gmail.com>
2020-06-17 23:59:13 +0530

    added belief propagation

--- scripts/my_MCL.py
+++ scripts/my_MCL.py
@@ -13,9 +13,12 @@ import numpy as np
 import time
 from math import sin, cos, atan2, pi, hypot
 
+T_MINUS_1 = 0
 NUM_OF_PARTICLES = 40
-PARTICLES = (np.random.rand(NUM_OF_PARTICLES * 3) * 6)- 3
-#PARTICLES = np.zeros(NUM_OF_PARTICLES * 3)
+START = 1
+PREV_CMD = [0, 0, 0]
+#PARTICLES = (np.random.rand(NUM_OF_PARTICLES * 3) * 6)- 3
+PARTICLES = np.zeros(NUM_OF_PARTICLES * 3)
 pose = [0.0, 0.0, 0.0]
 
 def cb_pose(data):
@@ -28,12 +31,14 @@ def cb_pose(data):
 
  
 def cb_mcl(data):
-    global pose, PARTICLES, NUM_OF_PARTICLES
-    vx  = data.linear.x;
-    vy  = data.linear.y;
-    w = data.angular.z;
-    v = hypot(vx, vy)
-    r = v / w
+    global pose, PARTICLES, NUM_OF_PARTICLES, T_MINUS_1, START, PREV_CMD
+    now = rospy.get_time()
+    delta_t = now - T_MINUS_1
+    if(START == 1):
+        delta_t = 0
+        START = 0
+    vel_x, vel_y, ang_vel = PREV_CMD[0], PREV_CMD[1], PREV_CMD[2]
+    lin_vel = hypot(vel_x, vel_y)
     x_n = np.zeros(NUM_OF_PARTICLES)
     y_n = np.zeros(NUM_OF_PARTICLES)
     phi_n = np.zeros(NUM_OF_PARTICLES)
@@ -41,13 +46,35 @@ def cb_mcl(data):
     x = p[0]
     y = p[1]
     phi = p[2]
-    for i in np.arange(NUM_OF_PARTICLES):
-        x_n[i] = x[i] - r * sin(phi[i]) + r * sin(phi[i] + w* 0.1)
-        y_n[i] = y[i] + r * cos(phi[i]) - r * cos(phi[i] + w* 0.1)
-        phi_n[i] = phi[i] + w* 0.1
-    PARTICLES = np.hstack((x_n,y_n,phi_n))
+    x = x + vel_x * np.cos(phi) * delta_t
+    y = y + vel_x * np.sin(phi) * delta_t
+    phi = phi - ang_vel * delta_t
+    #motion update
+    #for i in np.arange(NUM_OF_PARTICLES):
+       # if(np.abs(ang_vel)  0.001):
+       #     r = lin_vel / ang_vel
+       #     x_n[i] = x[i] - (r * sin(phi[i])) + r * sin(phi[i] + ang_vel * delta_t) 
+       #     y_n[i] = y[i] + (r * cos(phi[i])) - r * cos(phi[i] + ang_vel * delta_t)
+       # else:
+       #     rospy.loginfo(ang_vel)
+        #x_n[i]= x[i] + vel_x * np.cos(phi[i]) * delta_t
+        #y_n[i] = y[i] + vel_x * np.sin(phi[i]) * delta_t
+        #phi_n[i] = map_angle(phi[i] - ang_vel * delta_t)
+      #  theta = PARTICLES[2*NUM_OF_PARTICLES + i]
+      #  PARTICLES[i] = PARTICLES[i] + vel_x * np.cos(theta) * delta_t
+      #  PARTICLES[NUM_OF_PARTICLES + i] = PARTICLES[NUM_OF_PARTICLES + i] + vel_x * np.sin(theta) * delta_t
+      #  PARTICLES[2*NUM_OF_PARTICLES + i] = map_angle(theta - ang_vel * delta_t)
+        #x_n[i] = x[i] + 1 * np.cos(phi[i]) * delta_t
+        #y_n[i] = y[i] + 1 * np.sin(phi[i]) * delta_t
+        #phi_n[i] = phi[i] + 0.5 * delta_t
+    PARTICLES = np.hstack((x, y, phi))
+    #Storing motion commands
+    vel_x = data.linear.x
+    vel_y = data.linear.y
+    ang_vel = data.angular.z
+    PREV_CMD = [vel_x, vel_y, ang_vel]
+    T_MINUS_1 = now
 
-   
 
 def map_angle(theta):
     if(theta > pi):
@@ -61,9 +88,9 @@ def process():
     global PARTICLES
     rospy.init_node('Localiser_Node_MCL')
     pub = rospy.Publisher('/particles', numpy_msg(Floats), queue_size=10)
+    rospy.Subscriber('/cmd_vel', Twist, cb_mcl)
     rospy.Subscriber('/odom', Odometry, cb_pose)
-    rospy.Subscriber('/cmd_vel', Twist , cb_mcl)
-    rate = rospy.Rate(2) 
+    rate = rospy.Rate(1) 
     
     
     while not rospy.is_shutdown():
